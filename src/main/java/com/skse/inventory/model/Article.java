8caca291121a191edf95c2e9e59f206f1c462f8f
GeetUiPath <geet.agarwal@uipath.com>
2025-06-29 22:08:06 +0530

    Add few changes

--- src/main/java/com/skse/inventory/model/Article.java
+++ src/main/java/com/skse/inventory/model/Article.java
@@ -11,6 +11,7 @@ public class Article {
     private Long id;
 
     private String name;
+    private String description;
     private Double cuttingCost;
     private Double printingCost;
     private Double stitchingCost;
@@ -30,6 +31,14 @@ public class Article {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Double getCuttingCost() {
